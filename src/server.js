0c3ef2c4aa70e3b849b9dd4fbc523088e97e6ed0
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-11-16 23:25:08 +0100

    Use JS on server side

---
+++ src/server.js
@@ -0,0 +1,53 @@
+var express = require("express");
+var http = require('http');
+var path = require("path");
+var logger = require("morgan");
+
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var debug = require("debug")("react-express-template");
+
+require("babel/register");
+
+var dist = path.join(__dirname, '/../dist');
+
+var app = express();
+app.use(logger("dev"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express["static"](dist));
+
+app.set("port", process.env.PORT || 3000);
+
+app.use(function(req, res, next) {
+  var err;
+  err = new Error("Not Found");
+  err.status = 404;
+  return next(err);
+});
+
+if (app.get("env") === "development") {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    return res.send({
+      message: err.message,
+      status: err.status,
+      stack: err.stack
+    });
+  });
+}
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  return res.send({
+    message: err.message
+  });
+});
+
+server = http.createServer(app);
+
+server.listen(app.get("port"), function() {
+  return debug("Express server listening on port " + server.address().port);
+});
