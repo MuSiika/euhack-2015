076d8fce4706b9621c2d5f6d7e24e65b45ed7db9
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2015-11-17 09:39:33 +0100

    Interactive codeeditor

--- src/CodeEditor.jsx
+++ src/CodeEditor.jsx
@@ -7,9 +7,29 @@ require('brace/mode/javascript')
 require('brace/theme/github')
 
 export default React.createClass({
+
+  getInitialState: function() {
+    return { source : 'alert("cats!");' };
+  },
+
+  code: function( text ) {
+    this.setState( { source : text } );
+    return false;
+
+  },
+
+  script: function() {
+    console.log("scripting!");
+    var source = this.state.source;
+    console.log( source );
+    var f = eval( source );
+  },
+
   render: function() {
     return <div>
-      <AceEditor mode="javascript" theme="github" editorProps={{$blockScrolling: true}} />
+      <AceEditor mode="javascript" value={this.state.source} onChange={this.code} theme="github" editorProps={{$blockScrolling: true}} />
+      <button onClick={this.script}>Is this ok?</button>
+      <button onClick={this.script}>Submit changes</button>
     </div>
   }
 });
