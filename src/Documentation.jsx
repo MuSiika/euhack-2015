2ebdf15bfed84142e474b42f8f6eaf3341194aab
nyyti <nyyti@joutsa.pc.hiit.fi>
2015-11-17 20:56:50 +0200

    Documentation updated

--- src/Documentation.jsx
+++ src/Documentation.jsx
@@ -4,88 +4,96 @@ var brace  = require('brace');
 
 require('brace/mode/javascript')
 require('brace/theme/github')
+
 const Card = require('material-ui/lib/card/card');
 const CardTitle = require('material-ui/lib/card/card-title');
-
-const List = require('material-ui/lib/lists/list');
-const ListDivider = require('material-ui/lib/lists/list-divider')
-const ListItem = require('material-ui/lib/lists/list-item');
+const CardText = require('material-ui/lib/card/card-text');
+const CardHeader = require('material-ui/lib/card/card-header');
 
 export default React.createClass({
 
   render: function() {
     return <div>
-      <Card>
-        <CardTitle title="Documentation"/>
-        <List>
-          <ListItem
-          disabled={true}
-          primaryText="addNote(note)"
-          secondaryText={
-            <p>
-            note on note
-            </p>
-          }/>
 
-          <ListDivider/>
-          <ListItem 
-          disabled={true}
-          primaryText="addChord(chord)"
-          secondaryText={
-            <p>
-            note on note
-            </p>
-          }/>
+          <Card>
+            <CardTitle title="Documentation"/>
 
-          <ListDivider/>
-          <ListItem
-          disabled={true} 
-          primaryText="setOctave(newOctave)"
-          secondaryText={
-            <p>
-            note on note
-            </p>
-          }/>
+            <CardText>
+            <h4>addNote(note)</h4>
+            <p><i>note</i>: a single note as string. For example "f" or "F"</p>
+            Add a note to the song. For a flat note, use - ("F-") and for sharp note use # ("F#")
+            </CardText>
 
-          <ListDivider/>
-          <ListItem 
-          disabled={true}
-          primaryText="setDelay(newDelay)"
-          secondaryText={
-            <p>
-            note on note
-            </p>
-          }/>
+            <ListDivider/>
 
-          <ListDivider/>
-          <ListItem
-          disabled={true} 
-          primaryText="flatten(note)"
-          secondaryText={
-            <p>
-            note on note
-            </p>
-          }/>
-          <ListDivider/>
-          <ListItem
-          disabled={true} 
-          primaryText="sharpen(note)"
-          secondaryText={
-            <p>
-            note on note
-            </p>
-          }/>
+            <CardText>
+            <h4>addChord(chord)</h4>
+            <p><i>chord</i>: an array of notes as string. For example ["c", "e", "g"]</p>
 
-          <ListDivider/>
-          <ListItem 
-          disabled={true}
-          primaryText="play()"
-          secondaryText={
-            <p>
-            note on note
-            </p>
-          }/>
-        </List>
+            Add a chord to the song. Notes in the array are the notes forming the chord.
+            </CardText>
+
+            <ListDivider/>
+
+            <CardText>
+            <h4>setOctave(newOctave)</h4>
+            <p><i>newOctave</i>: an integer between -4 and 3 where 0 is the first octave.</p>
+            Set octave for the notes being added after setting the octave.
+            The octave stays until set otherwise.
+            </CardText>
+
+            <ListDivider/>
+
+            <CardText>
+            <h4>setDelay(newDelay)</h4>
+            <p><i>newDelay</i>: time as integer in milliseconds</p>
+            Set time waited between two notes.
+            </CardText>
+
+            <ListDivider/>
+
+            <CardText>
+            <h4>flatten(note)</h4>
+            <p><i>note</i>: a string representing a singe note</p>
+            Flatter all added notes that are the same as the note given as parameter after calling the function.
+            Flattening lasts until set otherwise.
+            </CardText>
+
+            <ListDivider/>
+
+            <CardText>
+            <h4>sharpen(note)</h4>
+            <p><i>note</i>: a string representing a singe note</p>
+            Sharpen all added notes that are the same as the note given as parameter after calling the function.
+            Sharpening lasts until set otherwise.
+            </CardText>
+
+            <ListDivider/>
+            <CardText>
+            <h4>resetFlatsAndSharpened()</h4>
+            Reverts all flats and sharps to normal.
+            </CardText>
+
+            <ListDivider/>
+            <CardText>
+            <h4>addBreak()</h4>
+            Adds a break with length of one note to the song.
+            </CardText>
+
+            <ListDivider/>
+
+            <CardText>
+            <h4>setDelay(newDelay)</h4>
+            <p><i>newDelay</i>: time as integer in milliseconds</p>
+            Set time waited between two notes.
+            </CardText>
+            <ListDivider/>
+
+            <CardText>
+            <h4>play()</h4>
+            Play the song.
+            </CardText>
+
       </Card>
     </div>
   }
