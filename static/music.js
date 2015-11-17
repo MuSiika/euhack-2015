c5d955f9f3764832dc0a73bc52f7783b2a891939
Noora Vainio <nooraevainio@gmail.com>
2015-11-17 16:39:15 +0200

    flatten/sharpen any note

--- static/music.js
+++ static/music.js
@@ -12,44 +12,61 @@ var octave = DEFAULT_OCTAVE;
 var instrument = DEFAULT_INSTRUMENT;
 
 var noteArray = {
+    'c-'    :   -1,
     'c'     :   0,
     'c#'    :   1,
+    'd-'    :   1, 
     'd'     :   2,
     'd#'    :   3,
+    'e-'    :   3,    
     'e'     :   4,
+    'f-'    :   4,  
     'f'     :   5,
     'f#'    :   6,
+    'g-'    :   6,
     'g'     :   7,
     'g#'    :   8,
+    'a-'    :   8,
     'a'     :   9,
     'a#'    :   10,
-    'b'     :   11
+    'b-'    :   10,
+    'b'     :   11,
+    'b#'    :   12
 };
 
 var isFlattedOrSharpened = {
+    'c-'    :   0,
     'c'     :   0,
     'c#'    :   0,
+    'd-'    :   0, 
     'd'     :   0,
     'd#'    :   0,
+    'e-'    :   0,    
     'e'     :   0,
+    'f-'    :   0,  
     'f'     :   0,
     'f#'    :   0,
+    'g-'    :   0,
     'g'     :   0,
     'g#'    :   0,
+    'a-'    :   0,
     'a'     :   0,
     'a#'    :   0,
-    'b'     :   0
+    'b-'    :   0,
+    'b'     :   0,
+    'b#'    :   0
 };
 
 function convertNoteToNumber(note){
     note = note.toLowerCase();
+    console.log(note);
     var noteNumber = noteArray[note] + ((octave + 1)*12);
-    if(noteNumber == 127 && isFlattedOrSharpened[note] > 0){
+    console.log(noteNumber);
+    noteNumber += isFlattedOrSharpened[note];
+    if(noteNumber > 127){
         alert("KÄÄÄK");
-    }else if(noteNumber == 0 && isFlattedOrSharpened[note] < 0){
+    }else if(noteNumber < 0){
         alert("GAAH");
-    }else{
-        noteNumber += isFlattedOrSharpened[note];
     }
 
     return noteNumber;
@@ -58,15 +75,24 @@ function convertNoteToNumber(note){
 // Add note to noteArray
 function addNote(note){
     note = convertNoteToNumber(note);
-    notes.push([note]);
+    if(note <= 127 && note >= 0){
+        notes.push([note]);
+    }   
     console.log(notes);
 }
 
 function addChord(chord){
+    var broken = false;
     for(var i = 0; i < chord.length; i++){
         chord[i] = convertNoteToNumber(chord[i]);
+        if(chord[i] > 127 || chord[i] < 0){ 
+            broken = true;
+        }   
     }
-    notes.push(chord);
+    if(!broken){ 
+        notes.push(chord);
+    }
+    
 }
 
 function flatten(note){
@@ -92,6 +118,7 @@ function setInstrument(newInstrument){
 
 // Play one note using soundfont
 function soundfontPlay(note){
+
     for(var i = 0; i < note.length; i++){
         T.soundfont.play(note[i]);
     }
@@ -123,17 +150,25 @@ function reset(){
     octave = DEFAULT_OCTAVE;
     instrument = DEFAULT_INSTRUMENT;
     isFlattedOrSharpened = {
+    'c-'    :   0,
     'c'     :   0,
     'c#'    :   0,
+    'd-'    :   0, 
     'd'     :   0,
     'd#'    :   0,
+    'e-'    :   0,    
     'e'     :   0,
+    'f-'    :   0,  
     'f'     :   0,
     'f#'    :   0,
+    'g-'    :   0,
     'g'     :   0,
     'g#'    :   0,
+    'a-'    :   0,
     'a'     :   0,
     'a#'    :   0,
-    'b'     :   0
-    };
+    'b-'    :   0,
+    'b'     :   0,
+    'b#'    :   0
+};
 }
